40960de76dcecca7e1be80c8a5e68c4842184936
Anas-Hoss <72859778+Anas-Hoss@users.noreply.github.com>
2021-09-20 14:52:47 +0200

    Update parseur.py

--- parseur.py
+++ parseur.py
@@ -4,8 +4,8 @@ Created on Sun Sep 19 19:47:53 2021
 
 @author: hossi
 """
-f=open("C:/Users/hossi/Desktop/centralisetroisneudtailleun.txt","r")
-f_new=open("C:/Users/hossi/Desktop/resultatcentralisetroisneudtailleun.txt","wt")
+f=open("centralisetroisneudtailleun.txt","r")
+f_new=open("resultatcentralisetroisneudtailleun.txt","wt")
 for s in f:
     f_new.write((((((s.replace(', (','; (')).replace(', ',',')).replace(': ',';')).replace('; ',';\n'))))
 #    f_new.write(s.replace(', ',','))
